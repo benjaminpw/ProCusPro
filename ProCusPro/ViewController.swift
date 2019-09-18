621bd83fb709b7bd4892ed50072b0800851067f3
benjopw <benyaminpurbowasito@gmail.com>
2019-09-18 19:41:36 +0700

    Initial Commit

---
+++ ProCusPro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProCusPro
+//
+//  Created by Benjamin Purbowasito on 18/09/19.
+//  Copyright © 2019 iosda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
